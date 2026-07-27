46b61578bd8a06be7756b9028184d334cfefe8ac
mvpJoKeR <71071019+hak553@users.noreply.github.com>
2026-07-27 14:18:46 +0300

    التحدي الثالث

---
+++ Lesson01/Quiz 3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====Quiz 3====");
+            string nameApp = "PC";
+
+            Console.WriteLine($"====={nameApp}=====");
+            string processor = "AMD";
+            string motherBoard = "ASUS";
+            string graphicCard = "RTX 2060 ";
+            string ssd = "1TB";
+
+            Console.WriteLine($"My Processor is {processor}");
+            Console.WriteLine($"My MotherBoard is {motherBoard}");
+            Console.WriteLine($"My GraphicCard is {graphicCard}");
+            Console.WriteLine($"My SSD is {ssd}");
+
+        }
+    }
+}
