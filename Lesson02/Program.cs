e85b3daa953f9ff551f3f5a4704c0375f22bb996
mvpJoKeR <71071019+hak553@users.noreply.github.com>
2026-07-27 13:47:49 +0300

    Add Lesson 2 source code
    Uploaded Program.cs for the second lesson to help students practice and review the code.

---
+++ Lesson02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<====|Quiz 2|====>");
+
+            Console.WriteLine("<====|Application|====>");
+            Console.WriteLine("|----------------------------------------------------------------------------------------------");
+            Console.WriteLine("Enter What you need Drink?");
+            Console.WriteLine("Coffe or tea or juice");
+            string drink = Console.ReadLine();
+            Console.WriteLine("|----------------------------------------------------------------------------------------------");
+            Console.WriteLine("What Size do you prefer ?");
+            Console.WriteLine("Small or medium or Large");
+            string size = Console.ReadLine();
+
+            Console.WriteLine("<====|OutPut|====>");
+            Console.WriteLine($"the {drink} is being prepared");
+            Console.WriteLine($"And it's size {size}");
+
+        }
+    }
+}
