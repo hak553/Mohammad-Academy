804aa7cf22c9b78ceb1d80de74e42c527ce4bace
mvpJoKeR <71071019+hak553@users.noreply.github.com>
2026-07-27 14:13:14 +0300

    التحدي الثاني

---
+++ Lesson01/Quiz 2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====Quiz 2====");
+            string name = "Mohammad";
+            int age = 20;
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("My age is " + name);
+        }
+    }
+}
