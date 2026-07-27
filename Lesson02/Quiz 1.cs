3526626a8816d76823dc8020b7e6343a997aa238
mvpJoKeR <71071019+hak553@users.noreply.github.com>
2026-07-27 14:03:00 +0300

    التحدي الاول

---
+++ Lesson02/Quiz 1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<====|Quiz 1|====>");
+            Console.WriteLine("What your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("What your age :");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"your name is {name} , your age is {age}");
+
+        }
+    }
+}
