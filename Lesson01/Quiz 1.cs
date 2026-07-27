08b16f52c1b0ef788fe273a4824a3eb3d8d7806b
mvpJoKeR <71071019+hak553@users.noreply.github.com>
2026-07-27 14:10:26 +0300

    التحدي الاول

---
+++ Lesson01/Quiz 1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My first project");
+            Console.WriteLine("My name is : Mohammad");
+            Console.WriteLine("My age is : 20");
+        }
+    }
+}
